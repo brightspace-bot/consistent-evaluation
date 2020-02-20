a314c26049ccf98859871332b7d6484225867d07
Dylan Mackenzie <Dylan.MacKenzie@D2L.com>
2020-02-20 11:33:00 -0500

    refact schema

--- test/store/schema.js
+++ test/store/schema.js
@@ -1,12 +1,3 @@
-import OverallScoreStore from '../../components/stores/overallScore';
-
-const enumerable = (obj, getter) => {
-	Object.defineProperty(obj, getter, { enumerable: true });
-};
-
-['overallScore', 'overallScoreTwo']
-	.forEach(getter => enumerable(OverallScoreStore, getter));
-
 export const createSchema = () => {
 	return {
 		scoresStore: {
--- test/store/utils.js
+++ test/store/utils.js
@@ -4,24 +4,12 @@ const hasChild = (obj) => {
 	return !(obj instanceof Array) && !!Object.keys(obj).length;
 };
 
-const isGetter = (obj, prop) => {
-	const getter = Object.getOwnPropertyDescriptor(obj, prop);
-	return (getter && getter.get);
-};
-
 export const validate = (schema, store) => {
-	for (const field in store) {
-		if (field !== 'root') {
-			if (hasChild(store[field])) {
-				validate(schema[field], store[field]);
-			} else {
-				assert.deepEqual(store[field], schema[field]);
-			}
-		}
-	}
-	for (const getter in Object.getPrototypeOf(store)) {
-		if (isGetter(store, getter)) {
-			assert.deepEqual(store[getter], schema[getter]);
+	for (const field in schema) {
+		if (hasChild(schema[field])) {
+			validate(schema[field], store[field]);
+		} else {
+			assert.deepEqual(store[field], schema[field]);
 		}
 	}
 };
