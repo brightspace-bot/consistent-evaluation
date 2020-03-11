0e08ffe5db0d67e4662c0412336cab4f04ca9171
Carol Busman <Carol.Busman@D2L.com>
2020-03-11 15:47:43 -0400

    Grabbing latest html editor version, adjusting tests as no longer need timeout.

--- test/consistent-evaluation.test.js
+++ test/consistent-evaluation.test.js
@@ -1,16 +1,11 @@
 import '../consistent-evaluation.js';
 import { expect, fixture, html } from '@open-wc/testing';
 
-function timeout(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 describe('d2l-consistent-evaluation', () => {
 
 	it('should pass all axe tests', async() => {
 		const el = await fixture(html`<d2l-consistent-evaluation></d2l-consistent-evaluation>`);
 		expect(el).to.be.accessible();
-		await timeout(1000);
 	});
 
 });
