5c3e883eea3435837db639d3cf63fe535561bd63
Dylan MacKenzie <dylan.mackenzie@d2l.com>
2020-04-30 14:11:58 -0400

    updated improper binding

--- components/right-panel/consistent-evaluation-grade-result.js
+++ components/right-panel/consistent-evaluation-grade-result.js
@@ -1,3 +1,4 @@
+import '@brightspace-ui-labs/grade-result/d2l-grade-result.js';
 import './consistent-evaluation-right-panel-block';
 import { html, LitElement } from 'lit-element';
 
@@ -6,7 +7,7 @@ export class ConsistentEvaluationGradeResult extends LitElement {
 	static get properties() {
 		return {
 			href: { type: String },
-			token: {type: String}
+			token: { type: String }
 		};
 	}
 
@@ -14,8 +15,8 @@ export class ConsistentEvaluationGradeResult extends LitElement {
 		return html`
 			<d2l-consistent-evaluation-right-panel-block title="Overall Grade">
 				<d2l-labs-d2l-grade-result
-					href=${this.href}
-					token=${this.token}
+					.href=${this.href}
+					.token=${this.token}
 					_hideTitle
 				></d2l-labs-d2l-grade-result>
 			</d2l-consistent-evaluation-right-panel-block>
