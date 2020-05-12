c4c27b12513da19c9a9e3f5e8e98ed5bc9eb87de
Dylan MacKenzie <dylan.mackenzie@d2l.com>
2020-05-12 11:35:57 -0400

    adding localization for footer

--- locales/en.js
+++ locales/en.js
@@ -1,6 +1,10 @@
 export const val = {
+	retract: 'Retract',
+	rubrics: 'Rubrics',
+	outcomes: 'Outcomes',
 	overallAverage: 'Overall Average',
 	overallFeedback: 'Overall Feedback',
-	rubrics: 'Rubrics',
-	outcomes: 'Outcomes'
+	publish: 'Publish',
+	saveDraft: 'Save Draft',
+	update: 'Update',
 };
