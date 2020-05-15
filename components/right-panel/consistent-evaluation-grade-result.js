dcdea937f2ad8c4c5c77c305f910ea965308d05c
Dylan MacKenzie <dylan.mackenzie@d2l.com>
2020-05-15 13:29:30 -0400

    adding additional event logging for debugging purposes

--- components/right-panel/consistent-evaluation-grade-result.js
+++ components/right-panel/consistent-evaluation-grade-result.js
@@ -11,6 +11,30 @@ export class ConsistentEvaluationGradeResult extends LitElement {
 		};
 	}
 
+	onInitializedSuccess(e) {
+		console.log('initialized success', e);
+	}
+
+	onInitializedError(e) {
+		console.log('initialized error', e);
+	}
+
+	onGradeUpdatedSuccess(e) {
+		console.log('grade updated success', e);
+	}
+
+	onGradeUpdatedError(e) {
+		console.log('grade updated error', e);
+	}
+
+	onGradeSavedSuccess(e) {
+		console.log('grade saved success', e);
+	}
+
+	onGradeSavedError(e) {
+		console.log('grade saved error', e);
+	}
+
 	render() {
 		return html`
 			<d2l-consistent-evaluation-right-panel-block title="Overall Grade">
@@ -18,6 +42,12 @@ export class ConsistentEvaluationGradeResult extends LitElement {
 					.href=${this.href}
 					.token=${this.token}
 					_hideTitle
+					@d2l-grade-result-initialized-success=${this.onInitializedSuccess}
+					@d2l-grade-result-initialized-error=${this.onInitializedError}
+					@d2l-grade-result-grade-updated-success=${this.onGradeUpdatedSuccess}
+					@d2l-grade-result-grade-updated-error=${this.onGradeUpdatedError}
+					@d2l-grade-result-grade-saved-success=${this.onGradeSavedSuccess}
+					@d2l-grade-result-grade-saved-error=${this.onGradeSavedError}
 				></d2l-labs-d2l-grade-result>
 			</d2l-consistent-evaluation-right-panel-block>
 		`;
