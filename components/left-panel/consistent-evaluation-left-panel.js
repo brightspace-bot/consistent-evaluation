e5a9601817e5d0f57ea6fc563c968afdc76e2ab5
Josh Roach <Josh.Roach@d2l.com>
2020-06-04 11:31:57 -0400

    remove t

--- components/left-panel/consistent-evaluation-left-panel.js
+++ components/left-panel/consistent-evaluation-left-panel.js
@@ -16,7 +16,7 @@ export class ConsistentEvaluationLeftPanel extends LitElement {
 		this._iframeSrc = `http://localhost:8000?${qs}`;
 		//this._iframeSrc = `//s.brightspace.com/apps/annotations-viewer/1.13.1/index.html?${qs}`;
 
-		this._token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjRlM2VjYWY3LTYyZTAtNDdkMS05NjU1LWE5NGNhZDQxYjU2OSJ9.eyJpc3MiOiJodHRwczovL2FwaS5icmlnaHRzcGFjZS5jb20vYXV0aCIsImF1ZCI6Imh0dHBzOi8vYXBpLmJyaWdodHNwYWNlLmNvbS9hdXRoL3Rva2VuIiwiZXhwIjoxNTkxMjg3NjE2LCJuYmYiOjE1OTEyODQwMTYsInN1YiI6IjE3NyIsInRlbmFudGlkIjoiNjE5ZjViZWEtODk4NS00ZTQxLThlYTUtMzAwZjk3NTk2ZmJiIiwiYXpwIjoiRXhwYW5kb0NsaWVudCIsInNjb3BlIjoiKjoqOioiLCJqdGkiOiI1ZDZhM2RjYS04YTU2LTQ0NGYtYTk0Yi01M2MyNjYwZjg0YjkifQ.KDx-hRUUAOeRAEvBpHSq1yWSHlIus9PkdZ3NVY8gXaG8LGji-aIk2oYi8OvsnIUydwx2AgHwI2sQs4cEDuW4YUXiLQcj61iaibDhXxqPyxQIuIfsHe45qe5dOGx7x96hbgJaWwOS6M8EBQdXFrTOLQIPimpgWJ7fk7eSJZ6f_-BT-FlPzwartmsVO6eUeuQbw77Byfo-BcriIFsuVDoAoh_5gDUrhQB2BwanmAhOhzMCgav7ybIV9WCputni-eFj2oAwoT2GRSEzWQvPVGJJ4xLuKTNuJNEY59U1NCaGdBkHtcBWO1-oQHoUUi_1hwQr0CPQySOZLm-4_7GDijZzxQ';
+		this._token = '';
 	}
 
 	render() {
