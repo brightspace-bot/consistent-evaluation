390d7fba8e77be2bde1aa323a80b270a99127bd0
Jamie Murray <Jamie.Murray@D2L.com>
2020-08-12 13:25:39 -0400

    - Single quotes

--- components/right-panel/consistent-evaluation-grade-result.js
+++ components/right-panel/consistent-evaluation-grade-result.js
@@ -129,7 +129,7 @@ export class ConsistentEvaluationGradeResult extends LocalizeMixin(LitElement) {
 		const dialogUrl = this.gradeItemInfo && this.gradeItemInfo.evaluationUrl;
 
 		if (!dialogUrl) {
-			console.error(`Consistent-Eval: Expected grade item evalutaion dialog URL, but none found`);
+			console.error('Consistent-Eval: Expected grade item evalutaion dialog URL, but none found');
 			return;
 		}
 
@@ -170,7 +170,7 @@ export class ConsistentEvaluationGradeResult extends LocalizeMixin(LitElement) {
 		const dialogUrl = this.gradeItemInfo && this.gradeItemInfo.statsUrl;
 
 		if (!dialogUrl) {
-			console.error(`Consistent-Eval: Expected grade item statistics dialog URL, but none found`);
+			console.error('Consistent-Eval: Expected grade item statistics dialog URL, but none found');
 			return;
 		}
 
