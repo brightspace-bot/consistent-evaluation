0465015efde91ccf123d78ce7985b4c782b384f2
Jamie Tran <Jamie.Tran@D2L.com>
2020-08-27 14:34:51 -0400

    :white_check_mark: Add visual diff test for rubric component

---
+++ test/perceptual/consistent-evaluation-rubric.visual-diff.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+const VisualDiff = require('@brightspace-ui/visual-diff');
+
+describe('d2l-consistent-evaluation', () => {
+
+	const visualDiff = new VisualDiff('consistent-evaluation-rubric', __dirname);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch();
+		page = await browser.newPage();
+		await page.setViewport({width: 900, height: 900, deviceScaleFactor: 2});
+		await page.goto(`${visualDiff.getBaseUrl()}/test/perceptual/consistent-evaluation-rubric.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
+		await page.bringToFront();
+		await visualDiff.disableAnimations(page);
+	});
+
+	after(async() => await browser.close());
+
+	it('renders rubric', async function() {
+		const rect = await visualDiff.getRect(page, '#default');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+});
