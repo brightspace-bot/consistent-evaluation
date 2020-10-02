2e5427c4597cac7bdf7b4d869a0fb50c1b918d80
Amaury Perez <Amaury.Perez@D2L.com>
2020-10-02 14:39:53 -0400

    US120317 - LCB - Exemptions and Late Subs - Handle if Learner is exempt - fixing demo page

--- components/header/d2l-consistent-evaluation-learner-context-bar.js
+++ components/header/d2l-consistent-evaluation-learner-context-bar.js
@@ -54,12 +54,20 @@ export class ConsistentEvaluationLearnerContextBar extends RtlMixin(LitElement) 
 		`;
 	}
 
+	_getIsExempt() {
+		if (this.submissionInfo && this.submissionInfo.isExempt) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	render() {
 		return html`
 			<d2l-consistent-evaluation-lcb-user-context
 				href="${this.href}"
 				.token="${this.token}"
-				?is-exempt="${this.submissionInfo.isExempt}"
+				?is-exempt="${this._getIsExempt()}"
 			></d2l-consistent-evaluation-lcb-user-context>
 		`;
 	}
