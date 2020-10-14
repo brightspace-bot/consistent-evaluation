aee8f361f25837b79af8440e35d78270d5190415
Alex Bedley <alex.bedley@D2L.com>
2020-10-14 11:49:34 -0400

    update and add more unit test for awaiter

--- test/awaiter.test.js
+++ test/awaiter.test.js
@@ -1,38 +1,80 @@
 import { assert } from '@open-wc/testing';
 import { Awaiter } from '../components/awaiter.js';
 
-class MyObject {
-	constructor(awaiter) {
-		this.val = 'this is my object';
-		this.awaiter = awaiter;
-	}
-	async change(newVal) {
-		const unlock = await this.awaiter.lock();
-		await new Promise((resolve) => {
-			this.val = newVal;
-			resolve();
-		});
-		unlock();
-		return Promise.resolve();
-	}
-}
-
 describe('Awaiter', () => {
 	it('should do nothing when there is only one lock', async() => {
 		const awaiter = new Awaiter();
-		const obj = new MyObject(awaiter);
-		await obj.change('changed object');
-		assert.equal(obj.val, 'changed object');
+		const unlock = await awaiter.lock();
+		await unlock();
 	});
 
 	it('should lock and unlock properly', async() => {
 		const awaiter = new Awaiter();
-		const obj1 = new MyObject(awaiter);
+		let num = 1;
+		const addOne = async() => {
+			const unlock = await awaiter.lock();
+			num++;
+			await unlock();
+		};
+
 		const unlock = await awaiter.lock();
-		const resolve = obj1.change('changed!');
-		assert.equal(obj1.val, 'this is my object');
+		const promise = addOne();
+		assert.equal(num, 1);
 		await unlock();
-		await resolve;
-		assert.equal(obj1.val, 'changed!');
+		await promise;
+		assert.equal(num, 2);
+	});
+
+	it('should dispatch', async() => {
+		const awaiter = new Awaiter();
+		let num = 1;
+
+		await awaiter.dispatch(() => {
+			num++;
+		});
+		assert.equal(num, 2);
+	});
+	it('should dispatch and return', async() => {
+		const awaiter = new Awaiter();
+		const val = 1;
+		const ret = await awaiter.dispatch(() => {
+			return val;
+		});
+		assert.equal(ret, val);
+	});
+	it('should dispatch multiple', async() => {
+		const awaiter = new Awaiter();
+		let num = 1;
+		const addOne = awaiter.dispatch(() => {
+			num++;
+		});
+		const addOneAgain = awaiter.dispatch(() => {
+			num++;
+		});
+		assert.equal(num, 1);
+		await addOne;
+		assert.equal(num, 2);
+		await addOneAgain;
+		assert.equal(num, 3);
+	});
+	it('dispatch should unlock when exception is thrown', async() => {
+		const awaiter = new Awaiter();
+		let num = 1;
+		const error = new Error('oops');
+		const throwError = awaiter.dispatch(() => {
+			throw error;
+		});
+		const addOne = awaiter.dispatch(() => {
+			num++;
+		});
+
+		try {
+			await throwError;
+		} catch (e) {
+			assert.equal(error, e);
+		}
+		assert.equal(num, 1);
+		await addOne;
+		assert.equal(num, 2);
 	});
 });
